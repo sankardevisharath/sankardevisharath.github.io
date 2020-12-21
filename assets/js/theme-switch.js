2b890c4135644ab18058e9586c8daf0615545835
Sarathkumar <s.sankaranarayanan@habibbank.com>
2020-12-22 00:30:36 +0400

    Fixed menu button for mobile devices.

--- assets/js/theme-switch.js
+++ assets/js/theme-switch.js
@@ -1,8 +1,5 @@
 $('.switch').click(()=>{
-    console.log("Clicked switch");
-    
     $("*[class*=dark],*[class*=light]").each((i, ele) =>{
-        console.log("dark " + ele.class);
         $(ele).toggleClass('bg-light bg-dark')
         $(ele).toggleClass('text-light text-dark')
         $(ele).toggleClass('navbar-light navbar-dark')
